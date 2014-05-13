cff5a9fce90b6b820bc0017364fde6366ff01825
CS <cstupi@debianVM>
2014-05-12 21:09:55 -0700

    working?

--- ec.c
+++ ec.c
@@ -2,7 +2,6 @@
 #include <time.h>
 #include <stdlib.h>
 #include <math.h>
-#define DEVICE_COUNT 20
 #define RUN_COUNT 10
 #define true 1
 #define false 0
@@ -16,12 +15,19 @@ Each iteration of the loop is 1 51.2us slot.
 If it is current somethings turn to go it will try and send.
 If collision happens it will then wait 2^k-1 slots to try again
 */
+typedef struct node 
+{
+	int initial;
+	int final;
+}node;
 
-int getSendCount(int array[DEVICE_COUNT][RUN_COUNT], int run, int currTime) // See how many want to send
+FILE *f;
+
+int getSendCount(int array[][RUN_COUNT], int run, int currTime, int device_count) // See how many want to send
 {
 	int i = 0;
 	int attemptingToSend = 0;
-	for(i; i < DEVICE_COUNT; i++)
+	for(i; i < device_count; i++)
 		if(array[i][run] == currTime)
 			attemptingToSend = attemptingToSend + 1;
 	return attemptingToSend;
@@ -39,10 +45,10 @@ int pow2(int exp) // Math library was giving issues so this calculates 2^exp
 {
 	return 1 << exp;
 }
-void collisionOccured(int array[DEVICE_COUNT][RUN_COUNT], int run,int currTime, int colCount) // Handle collision occuring
+void collisionOccured(int array[][RUN_COUNT], int run,int currTime, int colCount, int device_count) // Handle collision occuring
 {
 	int i = 0;
-	for(i; i < DEVICE_COUNT; i++)
+	for(i; i < device_count; i++)
 		if(array[i][run] == currTime){
 			array[i][run] = currTime + 1 + (rand() % pow2(colCount)); // Add 1 since collision took a slot
 		}
@@ -50,22 +56,27 @@ void collisionOccured(int array[DEVICE_COUNT][RUN_COUNT], int run,int currTime, 
 }
 
 // completed - set a new time
-void packetSent(int  array[DEVICE_COUNT][RUN_COUNT], int run, int currTime, int lambda)
+void packetSent(int  array[][RUN_COUNT],int start[][RUN_COUNT], int run, int currTime, int lambda, int device_count)
 {
 	int i = 0;
-	for(i; i < DEVICE_COUNT; i++)
-		if(array[i][run] == currTime)
+	for(i; i < device_count; i++)
+		if(array[i][run] == currTime) {
+			
+			fprintf(f,"%d\n",array[i][run] - start[i][run]);
+			
 			array[i][run] = currTime + PSIZE + calcRandIntervalBetweenTransmissionAttempts(lambda);
+			start[i][run] = array[i][run];
+		}
 }
-int checkFinished(int array[DEVICE_COUNT][RUN_COUNT], int run, int currTime) // Check if they have all finished
+int checkFinished(int array[][RUN_COUNT], int run, int currTime, int device_count) // Check if they have all finished
 {
 	int i = 0;
-	for(i; i < DEVICE_COUNT; i++)
+	for(i; i < device_count; i++)
 		if(array[i][run] >= currTime)
 			return false;
 	return true;
 }
-int calculateAverage(int array[DEVICE_COUNT][RUN_COUNT], int device) // Average out each row
+int calculateAverage(int array[][RUN_COUNT], int device) // Average out each row
 {
 	int i;
 	int average = 0;
@@ -74,11 +85,11 @@ int calculateAverage(int array[DEVICE_COUNT][RUN_COUNT], int device) // Average 
 	return average/RUN_COUNT;
 
 }
-void sortRunsByFinishTime(int array[DEVICE_COUNT][RUN_COUNT]) // Sorts array by finish time (col by col)
+void sortRunsByFinishTime(int array[][RUN_COUNT], int device_count) // Sorts array by finish time (col by col)
 {
 	int i, j,k;
 	for(k=0; k < RUN_COUNT; k++){ // Each run
-    for (i = 1; i < DEVICE_COUNT; i++) {
+    for (i = 1; i < device_count; i++) {
     	int tmp = array[i][k];
     	for (j = i; j >= 1 && tmp < array[j-1][k]; j--)
     	    	array[j][k] = array[j-1][k];
@@ -90,53 +101,61 @@ int main()
 {
 	time_t t;
 	srand((unsigned) time(&t));
-	int devices[DEVICE_COUNT][RUN_COUNT];
-	int jitter[RUN_COUNT];
+	int device_count, lambda, psize;
+	printf("Enter device count: ");
+	scanf("%d",&device_count);
+	printf("Enter lambda: ");
+	scanf("%d", &lambda);
+	printf("Enter packet size: ");
+	scanf("%d",&psize);
+	int devices[device_count][RUN_COUNT];
+	int start[device_count][RUN_COUNT];
 	int colCount[RUN_COUNT];
 	int runtime[RUN_COUNT];
 	int completedCount[RUN_COUNT];
-	int i=0,j=0, colcount, lambda, num_sending, blocked, completed;
-	printf("Enter lambda: ");
-	scanf("%d", &lambda);
+	int wastedSlots[RUN_COUNT];
+	int i=0,j=0, colcount, num_sending, blocked, completed;
+	
+f = fopen("results.csv","w");
+//	struct node** array = (struct node**)calloc(device_count+3,sizeof(struct node*));
+
+
 	for(j=0; j < RUN_COUNT; j++) // Initialize
-		for(i=0; i < DEVICE_COUNT; i++){
+		for(i=0; i < device_count; i++){
 			devices[i][j] = calcRandIntervalBetweenTransmissionAttempts(lambda);
+			start[i][j] = devices[i][j];
 			completedCount[i] = 0;
 			colCount[i] = 0;
 		}
 	for(j = 0; j < RUN_COUNT; j++, colcount=0){
-		jitter[j] = 0;
 		blocked = 0;
 		completedCount[j] = 0;
+		wastedSlots[j] = 0;
 		for(i = 0; i < RUNTIME; i++){
-			num_sending = getSendCount(devices, j, i);
-			if(num_sending > 0 && blocked > i) // Jitter?
+			num_sending = getSendCount(devices, j, i, device_count);
+			if(num_sending > 0 && blocked > i) 
 			{
-				collisionOccured(devices,j, i, colcount);
+				collisionOccured(devices,j, i, colcount, device_count);
 				colcount++;
-				jitter[j]++;
 			} else if(num_sending > 1){
-		//		printf("collision occuring in run #%d at time %d\n",j,i);
-	//			printf("num_sending = %d blocked = %d\n",num_sending,blocked);
-				collisionOccured(devices,j, i, colcount);
+				collisionOccured(devices,j, i, colcount, device_count);
 				colcount++;
 			} else if (num_sending == 1) { // 1 is sending .. block senders for 8 timeslots
-				blocked = i + PSIZE;
+				blocked = i + psize;
 				completedCount[j]++;
-				packetSent(devices,j,i,lambda);
-		//		printf("Blocked until %d\n",blocked);
+				packetSent(devices,start,j,i,lambda,device_count);
 				colCount[j] += colcount;
-				colcount = 1;
-			} else { // jitter occuring? -- timeslot is unused
-			//	jitter[j]++;
+				colcount = 0;
+			} else 
+			{
+				wastedSlots[j]++;
 			}
 		}
-		
-		runtime[j] = i;
 	}
+	fclose(f);
 	for(i = 0; i < RUN_COUNT; i++){
 		printf("Collision count for run %d: %d\n",i+1, colCount[i]);
-		printf("Jitter count for run %d: %d\n", i+1, jitter[i]);
-		printf("Completed count for run %d: %d\n\n",i+1, completedCount[i]);
+		printf("Completed count for run %d: %d\n",i+1, completedCount[i]);
+		printf("Total slots used: %d Total: %d\n\n", colCount[i] + completedCount[i], colCount[i]+completedCount[i]+wastedSlots[i]);
 	}
 }
